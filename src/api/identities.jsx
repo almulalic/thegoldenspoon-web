b279bdd7e1275f282aa01c638ce8c9a7ee2fe0d6
almulalic <almir.mulalic.am@gmail.com>
2020-04-25 23:43:45 +0200

    fixed server issues

--- src/api/identities.jsx
+++ src/api/identities.jsx
@@ -1,24 +1,29 @@
 import axios from "axios";
+import { BASE_API_URL } from "./index";
 
 export default {
   register: (body) =>
-    axios.post(`/identity/register`, body).catch((err) => {
+    axios.post(BASE_API_URL + `/identity/register`, body).catch((err) => {
       console.log(err);
     }),
   login: (body) =>
-    axios.post(`/identity/login`, body).catch((err) => {
+    axios.post(BASE_API_URL + `/identity/login`, body).catch((err) => {
       console.log(err);
     }),
   isUniqueEmail: (email) =>
-    axios.get(`/identity/isUniqueEmail/${email}`).catch((err) => {
-      console.log(err);
-    }),
+    axios
+      .get(BASE_API_URL + `/identity/isUniqueEmail/${email}`)
+      .catch((err) => {
+        console.log(err);
+      }),
   isUniqueUsername: (username) =>
-    axios.get(`/identity/isUniqueUsername/${username}`).catch((err) => {
-      console.log(err);
-    }),
+    axios
+      .get(BASE_API_URL + `/identity/isUniqueUsername/${username}`)
+      .catch((err) => {
+        console.log(err);
+      }),
   confirmation: (token) =>
-    axios.get(`/identity/confirmation/${token}`).catch((err) => {
+    axios.get(BASE_API_URL + `/identity/confirmation/${token}`).catch((err) => {
       console.log(err);
     }),
 };
