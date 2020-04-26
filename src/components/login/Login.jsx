28000e9f2ea3a13926ea67e2fa7e31056d4d8128
unknown <ema.arnautovic99@gmail.com>
2020-04-26 19:35:07 +0200

    forgot password, reset password, reset mail pages added

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -4,7 +4,6 @@ import identities from "../../api/identities";
 import { Password } from "primereact/password";
 import { Button } from "primereact/button";
 import { Stack } from "../../elements/stack/Stack";
-import { Card } from "primereact/card";
 import { Message } from "primereact/message";
 import {
   Inplace,
@@ -17,8 +16,6 @@ export default function Login() {
   // MAIN STATES
   const [loginEmailInput, setLoginEmailInput] = useState("");
   const [loginPasswordInput, setLoginPasswordInput] = useState("");
-  const [forgetPasswordEmailInput, setForgotPasswordInput] = useState("");
-  const [isForgetPasswordVisible, setForgetPasswordVisible] = useState(false);
 
   // LOADING STATES
   const [isLoadingResponse, setIsLoadingReposne] = useState(false);
@@ -120,29 +117,9 @@ export default function Login() {
           }}
         />
       </div>
-
-      <Inplace onOpen={isForgetPasswordVisible}>
-        <InplaceDisplay>
-          <span className="forgotPasswordText">Forgot your password ?</span>
-        </InplaceDisplay>
-        <InplaceContent>
-          <Stack vertical alignment="center">
-            <span className="p-float-label">
-              <InputText
-                id="primaryEmailAdress"
-                label="email"
-                value={forgetPasswordEmailInput}
-                onChange={(e) => setForgotPasswordInput(e.target.value)}
-                size="50"
-              />
-              <label htmlFor="float-input" className="loginLabel">
-                Primariy email adress
-              </label>
-            </span>
-            <Button label="Submit" />
-          </Stack>
-        </InplaceContent>
-      </Inplace>
+      <span>
+        <a href="">Forgot password?</a>
+      </span>
 
       <div>
         {userDoesentExistError ? (
---
+++ src/pages/resetPassword/ResetPassword.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Password } from "primereact/password";
+import { Button } from "primereact/button";
+import { Stack } from "../../elements/stack/Stack";
+import { Card } from "primereact/card";
+import "./ResetPassword.css";
+import identities from "../../api/identities";
+
+export default function ResetPassword() {
+  const [passwordInput, setPasswordInput] = useState("");
+  const [confirmPasswordInput, setConfirmPasswordInput] = useState("");
+  const footer = (
+    <span>
+      <Button className="button" label="Submit" />
+    </span>
+  );
+
+  //ERROR HANDLING
+  const [passwordNotStrongError, setPasswordNotStrongError] = useState(false);
+  /* for my next trick, I'll be a smart ass that tried to help:
+  const handleErrorClear = () => {
+  setPasswordDontMatchError(false);
+  setPasswordNotStrongError(false);
+  };
+
+  const ValidateForm = () => {
+    setIsValidatingFormData(true);
+    if (
+        passwordInput.length <= 0 ||
+      confirmPasswordInput.length <= 0 ||
+    ){
+        
+      setEmptyFieldsError(true);
+      setIsValidatingFormData(false);
+    }else (){
+        handleErrorClear();
+        return;
+    }
+
+    identities{
+        if (passwordInput !== confirmPasswordInput) {
+            handleErrorClear();
+            setPasswordDontMatchError(true);
+            setIsValidatingFormData(false);
+            return;
+          } else {
+            setIsValidatingFormData(false);
+          }
+    }
+    */
+
+  return (
+    <div className="main">
+      <Stack vertical alignment="center">
+        <Card
+          className="card"
+          footer={footer}
+          title="Reset your password"
+          subTitle="Enter a new password: "
+        >
+          <span className="p-float-label">
+            <Stack vertical distribution="fill">
+              <span className="p-float-label">
+                <Password
+                  id="password"
+                  label="password"
+                  value={passwordInput}
+                  onChange={(e) => setPasswordInput(e.target.value)}
+                  size="40"
+                />
+                <label htmlFor="float-input">Password</label>
+              </span>
+              <span className="p-float-label">
+                <Password
+                  id="password"
+                  label="password"
+                  value={confirmPasswordInput}
+                  onChange={(e) => setConfirmPasswordInput(e.target.value)}
+                  size="40"
+                  feedback={false}
+                />
+                <label htmlFor="float-input">Confirm password</label>
+              </span>
+            </Stack>
+          </span>
+        </Card>
+      </Stack>
+    </div>
+  );
+}
