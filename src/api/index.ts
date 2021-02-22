28bdad3b3fde3909c7caccc2486b8f642e12b293
almulalic <47641522+almulalic@users.noreply.github.com>
2021-02-22 14:00:17 +0100

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,1 @@
-export const BASE_API_URL = "https://thegoldenspoon-api.herokuapp.com";
-// export const BASE_API_URL = "http://localhost:5000";
+export const BASE_API_URL = "https://the-golden-spoon-api.herokuapp.com"
